2dc9e5efd667b103c2d72797382c544fcb5b8e28
Sreekara <syachama@purdue.edu>
2018-12-31 23:37:45 +0530

    search info passed down to app component

--- image-search-react/src/components/SearchBar.js
+++ image-search-react/src/components/SearchBar.js
@@ -4,10 +4,15 @@ class SearchBar extends React.Component {
 
     state = { term: '' };
 
+    onFormSubmit = event => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <label>Image Search</label>
                     <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })}></input>
                 </form>
